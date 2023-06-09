66edc868ac997acab4ddaf121d631903fe71c403
Murin Sergey <78379444+sergey971@users.noreply.github.com>
2023-06-09 08:55:33 +0400

    new form

--- js/main.js
+++ js/main.js
@@ -1,42 +1,15 @@
-function serializeForm(formNode) {
-  const { elements } = formNode;
-  const data = Array.from(elements)
-    .filter((item) => !!item.name)
-    .map((element) => {
-      const { name, value, type } = element;
-      
-      if (type === 'text') {
-        if (value.trim() === '') {
-          const errorMessage = formNode.querySelector(`#${name} + .errorMessage`);
-          errorMessage.textContent = 'Поле обязательно для заполнения';
-          element.classList.add('error');
-          
-        }
-      }
-      
-      if (type === 'number') {
-        if (!validateNumber(value)) {
-          const errorMessage = formNode.querySelector(`#${name} ~ .errorMessage`);
-          errorMessage.textContent = 'Некорректное значение';
-          element.classList.add('error');
-        }
-      }
-      
-      return { name, value, type };
-    });
+const form = document.getElementById('form');
 
-  console.log(data);
-}
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-  serializeForm(applicantForm);
-}
-
-function validateNumber(number) {
-  const numberRegex = /^\d+$/;
-  return numberRegex.test(number);
-}
-
-const applicantForm = document.getElementById('form');
-applicantForm.addEventListener('submit', handleFormSubmit);
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const fname = document.getElementById('fname').value.trim();
+    const lname = document.getElementById('lname').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const birthday = document.getElementById('birthday').value.trim();
+    
+    console.log(fname);
+    console.log(lname);
+    console.log(email);
+    console.log(birthday);
+});
