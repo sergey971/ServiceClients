048a8d8cc017625f7ef95870507f25dce39976be
Murin Sergey <78379444+sergey971@users.noreply.github.com>
2023-06-12 23:22:32 +0400

    add new settings

--- js/main.js
+++ js/main.js
@@ -1,15 +1,39 @@
 const form = document.getElementById('form');
+function validation(form){
 
+    function removeError(input){
+        const parent = input.parentNode;
+        if(parent.classList.contains('error')){
+            parent.querySelector('.errorMessage').remove();
+            parent.classList.remove('error');
+        }
+    }
+    function createError(input, text){
+        const parent = input.parentNode;
+        const errorText = document.createElement('p');
+        errorText.classList.add('errorMessage');
+        errorText.textContent = text;
+        parent.classList.add('error');
+        parent.append(errorText);
+    }
+
+    let result = true;
+
+    const allInputs = form.querySelectorAll('input');
+    for (const input of allInputs){
+        removeError(input);   
+        if(input.value == ""){
+            console.log('Ошибка Форма не отправленна');
+            createError(input, 'Поле не заполненно')
+            result = false;
+        }
+    }
+        
+    return result;
+}
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    
-    const fname = document.getElementById('fname').value.trim();
-    const lname = document.getElementById('lname').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const birthday = document.getElementById('birthday').value.trim();
-    
-    console.log(fname);
-    console.log(lname);
-    console.log(email);
-    console.log(birthday);
+    if(validation(this) === true){
+        alert('OK');
+    }
 });
